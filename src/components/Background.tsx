1b658cce77861b6ffaf38c846307ba583ca5b4c1
DominicDev <s0l0estudi4r@gmail.com>
2025-08-03 11:55:42 +0200

    v0.0.1

---
+++ src/components/Background.tsx
@@ -0,0 +1,10 @@
+import '@/styles/Background.css';
+import GlassFolder from '@/components/GlassFolder';
+
+export const Background = () => {
+  return (
+    <div className='background-effect'>
+      <GlassFolder />
+    </div>
+  );
+};
